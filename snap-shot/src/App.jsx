79add8237edbd4de61437544458b9f56c19097e4
Siyi Ji <sylviajsyy@gmail.com>
2025-12-08 22:54:41 -0700

    Have a child

--- snap-shot/src/App.jsx
+++ snap-shot/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import SearchField from './components/SearchField'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='w-screen'>
-      <Header />
+      <Header>
+        <SearchField />
+      </Header>
     </div>
         
   )
--- snap-shot/src/components/Header.jsx
+++ snap-shot/src/components/Header.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import SearchField from './SearchField'
 
-const Header = () => {
+const Header = ({ children }) => {
   return (
     <div className='bg-gray-900 flex items-center py-4'>
         <div className='max-w-md mx-auto w-full'>
             <h1 className='text-white text-center text-2xl font-bold mb-5'>Photo Snap</h1>
-            <SearchField />
+            { children }
         </div>
-      
     </div>
   )
 }
