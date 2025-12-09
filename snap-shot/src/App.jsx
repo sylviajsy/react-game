310817d5a0cee4412318f4bea04ab82ce272edb8
Siyi Ji <sylviajsyy@gmail.com>
2025-12-08 18:27:23 -0700

    Adding title

--- snap-shot/src/App.jsx
+++ snap-shot/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Nav from './components/nav'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <div>
-        <h1>
-          Image Search APP
-        </h1>
-      </div>
+    <div className='w-screen'>
+      <Nav />
+    </div>
+        
   )
 }
 
---
+++ snap-shot/src/components/nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='bg-gray-900 flex items-center py-4'>
+        <div className='max-w-md mx-auto w-full'>
+            <h1 className='text-white text-center text-2xl font-bold mb-5'>Photo Snap</h1>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Nav
