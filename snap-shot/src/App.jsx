1b16461db1d45710b10e1f5f84e18026d9ff9cbf
Siyi Ji <sylviajsyy@gmail.com>
2025-12-08 20:21:28 -0700

    Add css styling

--- snap-shot/src/App.jsx
+++ snap-shot/src/App.jsx
@@ -25,7 +25,7 @@ export default App
 // - Fetch logic
 
 // Child Component
-// 1. Search
+// 1. Header -> Search
 // 2. Show PhotoGrid
 // 3. Photos found successfully
 // 4. No result found
--- snap-shot/src/components/SearchField.jsx
+++ snap-shot/src/components/SearchField.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const SearchField = () => {
   return (
-    <div>
-      <input type='search' />
-      <button>
+    <div className='flex'>
+      <input className='bg-gray-50 border border-gray-300 text-sm w-full
+                        indent-2 p-2.5 placeholder-gray-500 text-gray-900'
+        type='search' 
+        placeholder='Search Anything...'/>
+      <button
+        className='bg-blue-600 px-6 py-2.5 text-white'>
         Search
       </button>
     </div>
