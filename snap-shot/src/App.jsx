90f406e0116ac13dad9165775ca3f0a95f75d6a0
Siyi Ji <sylviajsyy@gmail.com>
2025-12-09 00:31:18 -0700

    async to fetch data

--- snap-shot/src/App.jsx
+++ snap-shot/src/App.jsx
@@ -1,11 +1,23 @@
-import { useRef, useState } from 'react'
-import { Form, Button,InputGroup } from 'react-bootstrap';
+import { useState } from 'react'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SearchFiled from './components/SearchFiled';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [image, setImage] = useState([]);
+
+  const fetchData = async(searchTerm) => {
+    const access_key = import .meta.env.UNSPLASH_ACCESS_KEY;
+    try {
+      const response = await fetch(
+        `https://api.unsplash.com/search/photos?client_id=${access_key}&query=${searchTerm}`
+      );
+      const data = await response.json();
+      setImage(data);
+    } catch (error){
+      console.warn(error);
+    }
+  }
   
   return (
     <div className='Container'>
