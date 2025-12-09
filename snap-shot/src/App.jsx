0409f77e5d7888b2964812278c9a8915b4e1be53
Siyi Ji <sylviajsyy@gmail.com>
2025-12-08 23:06:13 -0700

    Add .env

--- snap-shot/src/App.jsx
+++ snap-shot/src/App.jsx
@@ -2,17 +2,18 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import SearchField from './components/SearchField'
+import Images from './components/Images'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='w-screen'>
+    <>
       <Header>
         <SearchField />
       </Header>
-    </div>
-        
+      <Images />
+    </>
   )
 }
 
---
+++ snap-shot/src/components/Images.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Images = () => {
+  return (
+    <div>
+      <h1 className='text-center underline mt-6 text-2xl'> 
+        Results for
+      </h1>
+      <div className='grid'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Images
